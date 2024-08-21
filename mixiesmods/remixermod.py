8f33659693e6c22a4dc0328daf479e2ca0b1e998
milkalotl <milkalotlalotl@gmail.com>
2024-08-21 23:46:34 +0200

    added quit() and made setup not setup whenever main is run

--- mixiesmods/remixermod.py
+++ mixiesmods/remixermod.py
@@ -5,25 +5,29 @@ def remixer(templocation,textslocation):
     if input("Press enter to start ★ remixing ★ !!! Press 0 to cancel and go back : ") == "0":
         return
 
-    fileinput = input("Write your filename please (no.txt) !!!: ")
-    filemane = f"{textslocation}{fileinput}.txt"
-    enco = input("and the encoding !!! Choose Latin-1 (1), UTF-8 (2), or custom (3) !!!: ")
-
+    def askforinput(textslocation, ):
+        x = input("Write your filename please (no.txt) !!!: ")
+        y = f"{textslocation}{x}.txt"
+        z = input("and the encoding !!! Choose Latin-1 (1), UTF-8 (2), or custom (3) !!!: ")
+        return y,z
+    filemane, enco = askforinput(textslocation)
+    
+    
 
     if enco == "1":
-        realenco = "latin1"
+        enco = "latin1"
     elif enco == "2":
-        realenco = "utf8"
+        enco = "utf8"
     else:
-        realenco = input("Please specify: ")
+        enco = input("Please specify: ")
 
     try:
-        open(filemane, encoding=realenco)
+        open(filemane, encoding=enco)
     except:
         print("oops wrong file name or encoding! Try again !!!")
         remixer(templocation,textslocation)
     else:
-        text = open(filemane, encoding=realenco).read()
+        text = open(filemane, encoding=enco).read()
 
     question = input("please specify if you want line breaks (1), no line breaks (2), or spaces AND linebreaks (3): ")
 
