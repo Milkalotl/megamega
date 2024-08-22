8a3eddf00461411e03e6fce9d12b49a3bcd55509
milkalotl <milkalotlalotl@gmail.com>
2024-08-22 22:54:27 +0200

    removed unnecesary files

--- tests/newertest.py
+++
@@ -1,9 +0,0 @@
-uni = 40
-while uni < 1000:
-    x = chr(uni)
-    try:
-        freq.get(x)
-    except:
-        count = freq.get(x)
-        print(f"{x}: {count}, {round((count / lengthoftxt * 100), 6)}%")
-    uni += 1
